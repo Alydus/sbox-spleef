0fb3640e759625ea9bfd0527d964fc5da222972f
Alydus <11297569+Alydus@users.noreply.github.com>
2022-08-17 22:28:22 +0100

    Implement SpleefThirdPersonCamera

--- code/Player/Camera/SpleefThirdPersonCamera.cs
+++ code/Player/Camera/SpleefThirdPersonCamera.cs
@@ -2,8 +2,36 @@
 
 namespace Alydus.Spleef
 {
-	class SpleefThirdPersonCamera : Sandbox.ThirdPersonCamera
+	class SpleefThirdPersonCamera : Sandbox.CameraMode
 	{
-		private float orbitDistance = 400;
+		public override void Update()
+		{
+			if ( Local.Pawn is not AnimatedEntity pawn )
+				return;
+
+			Position = pawn.Position;
+			Vector3 targetPos;
+
+			var center = pawn.Position + Vector3.Up * 64;
+
+			Position = center;
+			Rotation = Rotation.FromAxis( Vector3.Up, 4 ) * Input.Rotation;
+
+			float distance = 200.0f * pawn.Scale;
+			targetPos = Position + Input.Rotation.Right * (pawn.CollisionBounds.Maxs * pawn.Scale);
+			targetPos += Input.Rotation.Forward * -distance;
+
+			var tr = Trace.Ray( Position, targetPos )
+				.WithAnyTags( "solid" )
+				.Ignore( pawn )
+				.Radius( 8 )
+				.Run();
+
+			Position = tr.EndPosition;
+
+			FieldOfView = 70;
+
+			Viewer = null;
+		}
 	}
 }
