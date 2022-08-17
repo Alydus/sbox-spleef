14d995219b462d84d7cae608813468db62ed5728
Alydus <11297569+Alydus@users.noreply.github.com>
2022-08-17 23:56:22 +0100

    Implement nametags

---
+++ code/ui/NameTag.cs
@@ -0,0 +1,93 @@
+﻿using Alydus.Spleef;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using System.Linq;
+
+/// <summary>
+/// When a player is within radius of the camera we add this to their entity.
+/// We remove it again when they go out of range.
+/// </summary>
+internal class NameTagComponent : EntityComponent<SpleefPlayer>
+{
+	NameTag NameTag;
+
+	protected override void OnActivate()
+	{
+		NameTag = new NameTag( Entity.Client?.Name ?? Entity.Name, Entity.Client?.PlayerId );
+	}
+
+	protected override void OnDeactivate()
+	{
+		NameTag?.Delete();
+		NameTag = null;
+	}
+
+	/// <summary>
+	/// Called for every tag, while it's active
+	/// </summary>
+	[Event.Frame]
+	public void FrameUpdate()
+	{
+		var tx = Entity.GetAttachment( "hat" ) ?? Entity.Transform;
+		tx.Position += Vector3.Up * 10.0f;
+		tx.Rotation = Rotation.LookAt( -CurrentView.Rotation.Forward );
+
+		NameTag.Transform = tx;
+	}
+
+	/// <summary>
+	/// Called once per frame to manage component creation/deletion
+	/// </summary>
+	[Event.Frame]
+	public static void SystemUpdate()
+	{
+		foreach ( var player in Sandbox.Entity.All.OfType<SpleefPlayer>() )
+		{
+			if ( player.IsLocalPawn && player.IsFirstPersonMode )
+			{
+				var c = player.Components.Get<NameTagComponent>();
+				c?.Remove();
+				continue;
+			}
+
+			var shouldRemove = player.Position.Distance( CurrentView.Position ) > 500;
+			shouldRemove = shouldRemove || player.LifeState != LifeState.Alive;
+			shouldRemove = shouldRemove || player.IsDormant;
+
+			if ( shouldRemove )
+			{
+				var c = player.Components.Get<NameTagComponent>();
+				c?.Remove();
+				continue;
+			}
+
+			// Add a component if it doesn't have one
+			player.Components.GetOrCreate<NameTagComponent>();
+		}
+	}
+}
+
+/// <summary>
+/// A nametag panel in the world
+/// </summary>
+public class NameTag : WorldPanel
+{
+	public Panel Avatar;
+	public Label NameLabel;
+
+	internal NameTag( string title, long? steamid )
+	{
+		StyleSheet.Load("/ui/NameTag.scss");
+
+		if ( steamid != null )
+		{
+			Avatar = Add.Panel( "avatar" );
+			Avatar.Style.SetBackgroundImage( $"avatar:{steamid}" );
+		}
+
+		NameLabel = Add.Label( title, "title" );
+
+		PanelBounds = new Rect( -500, -100, 1000, 200 );
+	}
+}
